5c2e4fedd96191245f3555442fc602298361fcdf
ogershony <ogershony@gmail.com>
2025-11-23 22:47:21 -0800

    Added visualization logic to training script, changed requirements.txt to become separate

--- locomotion/env_test.py
+++ locomotion/env_test.py
@@ -13,5 +13,6 @@ xml_path = 'bittle_adapted_scene.xml'
 try:
     env = envs.get_environment(env_name, xml_path = xml_path)
     print(f"Successfully built environment {env_name} from {xml_path}")
-except:
+except Exception as e:
     print(f"Failed to build environment {env_name} from {xml_path}")
+    print(e)
---
+++ locomotion/training_helpers.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import jax
+
+x_data = []
+y_data = []
+ydataerr = []
+times = [datetime.now()]
+
+max_y, min_y = 40, 0
+
+'''
+Callback function to plot training progress.
+'''
+def progress(num_steps, metrics):
+  times.append(datetime.now())
+  x_data.append(num_steps)
+  y_data.append(metrics['eval/episode_reward'])
+  ydataerr.append(metrics['eval/episode_reward_std'])
+
+  plt.xlim([0, train_fn.keywords['num_timesteps'] * 1.25])
+  plt.ylim([min_y, max_y])
+
+  plt.xlabel('# environment steps')
+  plt.ylabel('reward per episode')
+  plt.title(f'y={y_data[-1]:.3f}')
+
+  plt.errorbar(
+      x_data, y_data, yerr=ydataerr)
+  plt.show()
+
+'''
+Domain randomization for accurate sim-to-real transfer.
+'''
+def domain_randomize(sys, rng):
+  """Randomizes the mjx.Model."""
+  @jax.vmap
+  def rand(rng):
+    _, key = jax.random.split(rng, 2)
+    # friction
+    friction = jax.random.uniform(key, (1,), minval=0.6, maxval=1.4)
+    friction = sys.geom_friction.at[:, 0].set(friction)
+    # actuator
+    _, key = jax.random.split(key, 2)
+    gain_range = (-5, 5)
+    param = jax.random.uniform(
+        key, (1,), minval=gain_range[0], maxval=gain_range[1]
+    ) + sys.actuator_gainprm[:, 0]
+    gain = sys.actuator_gainprm.at[:, 0].set(param)
+    bias = sys.actuator_biasprm.at[:, 1].set(-param)
+    return friction, gain, bias
+
+  friction, gain, bias = rand(rng)
+
+  in_axes = jax.tree_util.tree_map(lambda x: None, sys)
+  in_axes = in_axes.tree_replace({
+      'geom_friction': 0,
+      'actuator_gainprm': 0,
+      'actuator_biasprm': 0,
+  })
+
+  sys = sys.tree_replace({
+      'geom_friction': friction,
+      'actuator_gainprm': gain,
+      'actuator_biasprm': bias,
+  })
+  return sys, in_axes
